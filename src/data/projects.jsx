7ca0f89b45cce7713072d3b09422cc6101af6b0e
sarath nettem <sarath@avantari.org>
2022-05-21 13:08:41 +0530

    fix (project-data)

---
+++ src/data/projects.jsx
@@ -0,0 +1,23 @@
+import project1 from "../images/assets/project1.png";
+import project2 from "../images/assets/project2.png";
+
+export const ProjectsData = [
+    {
+        id: 1,
+        title: "Project1",
+        about: "This is the first project fro demonstration for protfolio site expaling the tools and programing languages that i have used to implement it",
+        tags: ["ReactJS", "NodeJS", "Mongodb"],
+        demo: "www.google.com",
+        github: "www.github.com",
+        image: project1
+    },
+    {
+        id: 2,
+        title: "Project2",
+        about: "This is the first project fro demonstration for protfolio site expaling the tools and programing languages that i have used to implement it",
+        tags: ["ReactJS", "NodeJS","ReactJS", "NodeJS","ReactJS", "NodeJS","Health Care", "Real Estate","ReactJS", "NodeJS","Business Site Purpose", "NodeJS",],
+        demo: "www.google.com",
+        github: "www.github.com",
+        image: project2
+    }
+];
