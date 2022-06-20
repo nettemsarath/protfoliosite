bd804f432a3d3d4ed5b33da678dc92f4c8541c20
sarath nettem <sarath@avantari.org>
2022-06-20 21:41:12 +0530

    style ( changes in contact styling )

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -4,20 +4,26 @@ import project2 from "../images/assets/project2.png";
 export const ProjectsData = [
     {
         id: 1,
-        title: "Project1",
+        title: "smartdhyana",
         about: "This is the first project fro demonstration for protfolio site expaling the tools and programing languages that i have used to implement it",
-        tags: ["ReactJS", "NodeJS", "Mongodb"],
+        tags: ["ReactJS", "NodeJS", "Mongodb", "Docker", "AWS", "Stripe", "nginx", "Socket.IO", "Multer"],
         demo: "www.google.com",
-        github: "www.github.com",
         image: project1
     },
     {
         id: 2,
+        title: "Decs.ai",
+        about: "This is the first project fro demonstration for protfolio site expaling the tools and programing languages that i have used to implement it",
+        tags: ["ReactJS", "NodeJS", "Mongodb"],
+        demo: "www.decs.ai",
+        image: project1
+    },
+    {
+        id: 3,
         title: "Project2",
         about: "This is the first project fro demonstration for protfolio site expaling the tools and programing languages that i have used to implement it",
         tags: ["ReactJS", "NodeJS","ReactJS", "NodeJS","ReactJS", "NodeJS","Health Care", "Real Estate","ReactJS", "NodeJS","Business Site Purpose", "NodeJS",],
         demo: "www.google.com",
-        github: "www.github.com",
         image: project2
     }
 ];
