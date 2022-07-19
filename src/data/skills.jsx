3a4ffc248d742ed98b90750cb5b5945be9230f04
sarath nettem <sarath@avantari.org>
2022-07-19 11:49:59 +0530

    feat (skills and add doamin name)

--- src/data/skills.jsx
+++ src/data/skills.jsx
@@ -1,6 +1,6 @@
 
 export const SkillsData = {
-    FrameWorks: "NodeJS ( ExpressJS ), Reactjs and Redux-toolKit, MongoDB ( NoSql Database ), Docker, Nginx ",
-    AWS_Services: "AWS Services ( EC2 instance,  S3 Bucket, SES,  Lambda using serverless Framework,  Route53,  Load-balancer , Cognito,  cloudFront  )",
-    Others: "payment gateway integration , Database Backups, Centralized Error Log monitoring (using Appsignal)"
+    FrameWorks: "NodeJS ( ExpressJS ), Reactjs and Redux-toolKit, MongoDB ( NoSql Database ), Docker, Nginx, Redis, Github Actions ",
+    AWS_Services: "AWS Services ( EC2 instance, S3 Bucket, SES, Lambda using serverless Framework, Route53, Load-balancer, Cognito, cloudFront  )",
+    Others: "payment gateway integration (Stripe, Paypal, Instamojo) , Database Backups, Centralized Error Log monitoring (using Appsignal)"
 }
