e5cb04a77acff1fd453ce45490acf42fdbbb782d
nettemsarath <32771824+nettemsarath@users.noreply.github.com>
2023-06-12 11:34:36 +0530

    fix: updated the resume link
    changed the google drive resume link

--- src/data/contact.jsx
+++ src/data/contact.jsx
@@ -1,2 +1,2 @@
 
-export const resumeLink = "https://docs.google.com/document/d/1r5nbf11sCviz4H_IyvGlwgQtFsVwaje2CfzmzMz69pk";
+export const resumeLink = "https://docs.google.com/document/d/1ruYWqXfAm9y2bQKJgUy1nQkFxW0EbuTJ291Abcq-uRs/edit?usp=sharing"
